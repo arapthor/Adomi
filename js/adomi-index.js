ff169a611f1e8d6326b1542004017dabd896a22b
arapthor <aangelamador10@gmail.com>
2020-07-01 22:54:58 -0430

    version los mas responsive posible 1.Master

--- js/adomi-index.js
+++ js/adomi-index.js
@@ -1,11 +1,13 @@
 /*   Desplazamiento del Form *//*
 document.querySelector('.pri').addEventListener('click', () =>{document.querySelector('.despla').classList.toggle('zable');});*/
+
 /*  Despliegue del nav  */
 document.querySelector('.Marcas').addEventListener('click', () =>{document.querySelector('.lasMarcas').classList.toggle('show'); document.querySelector('.Marcas').classList.toggle('point');});
 
 document.querySelector('.Productos').addEventListener('click', () =>{document.querySelector('.losProductos').classList.toggle('show'); document.querySelector('.Productos').classList.toggle('point');});
 
 document.querySelector('.Ayuda').addEventListener('click', () =>{document.querySelector('.laAyuda').classList.toggle('show'); document.querySelector('.Ayuda').classList.toggle('point');});
+
 /*  Despliegue de Categorias-Departamentos  */
 document.querySelector('.comoamazon').addEventListener('click', () =>{ document.querySelector('.envoltura').classList.toggle('noshow');});
 
