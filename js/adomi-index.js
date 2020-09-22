4768e038d85a76aa675704f99109223af2ab9153
arapthor <aangelamador10@gmail.com>
2020-09-21 21:38:07 -0400

    Adomi 2.0

--- js/adomi-index.js
+++ js/adomi-index.js
@@ -2,4 +2,7 @@
 document.querySelector('.depboton').addEventListener('click', () =>{ document.querySelector('.envoltura').classList.toggle('show'); document.querySelector('.depboton').classList.toggle('show');});
 
 /*  Menu de mobile (boton)*/
-document.querySelector('.mobmenu').addEventListener('click', () =>{document.querySelector('body').classList.toggle('active');})
+document.querySelector('.mobmenu').addEventListener('click', () =>{document.querySelector('body').classList.toggle('active');});
+
+/* Colors */
+document.querySelector('.coverfrecuentes span').addEventListener('click', () =>{document.querySelector('.cuadro').classList.toggle('color');});
